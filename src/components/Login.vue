90093f74f72c15d641c9bf1a77e775fa728aee31
haoran <haoran.mc@outlook.com>
2021-08-23 21:40:33 +0800

    :rocket:Reset form info

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -7,7 +7,7 @@
       </div>
 
       <!-- 登录表单区域 -->
-      <el-form :model="loginForm" :rules="loginFormRules" label-width="0px" class="login_form">
+      <el-form ref="loginFormRef" :model="loginForm" :rules="loginFormRules" label-width="0px" class="login_form">
         <!-- 用户名 -->
         <el-form-item prop="username">
           <el-input v-model="loginForm.username" prefix-icon="iconfont icon-user"></el-input>
@@ -21,7 +21,7 @@
         <!-- 按钮 -->
         <el-form-item class="btns">
           <el-button type="primary">登录</el-button>
-          <el-button type="info">重置</el-button>
+          <el-button type="info" @click="resetLoginForm">重置</el-button>
         </el-form-item>
       </el-form>
     </div>
@@ -49,6 +49,12 @@
             { min: 6, max: 15, message: "长度在 6 到 15 个字符", trigger: "blur" }
           ]
         }
+      }
+    },
+    methods: {
+      resetLoginForm () {
+        /* console.log(this) */
+        this.$refs.loginFormRef.resetFields()
       }
     }
   }
