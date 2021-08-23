834a45aab4143dfb68f9d858aeeda29f29fc8c9c
haoran <haoran.mc@outlook.com>
2021-08-23 20:45:02 +0800

    :construction:Data binding

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -7,15 +7,15 @@
       </div>
 
       <!-- 登录表单区域 -->
-      <el-form label-width="0px" class="login_form">
+      <el-form :model="loginForm" label-width="0px" class="login_form">
         <!-- 用户名 -->
         <el-form-item>
-          <el-input prefix-icon="iconfont icon-user"></el-input>
+          <el-input v-model="loginForm.username" prefix-icon="iconfont icon-user"></el-input>
         </el-form-item>
 
         <!-- 密码 -->
         <el-form-item>
-          <el-input prefix-icon="iconfont icon-3702mima"></el-input>
+          <el-input v-model="loginForm.password" prefix-icon="iconfont icon-3702mima" type="password"></el-input>
         </el-form-item>
 
         <!-- 按钮 -->
@@ -30,7 +30,16 @@
 
 <script>
   export default {
-    name: "Login"
+    name: "Login",
+    data () {
+      return {
+        // 登录表单的数据绑定对象
+        loginForm: {
+          username: '',
+          password: ''
+        }
+      }
+    }
   }
 </script>
 
