6f0bc7425fc5569a530ecd9b78502e6742b956c7
haoran <haoran.mc@outlook.com>
2021-08-24 11:24:28 +0800

    :lipstick:Home entirety style

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,8 +1,18 @@
 <template>
-  <div>
-    <el-button type="info" @click="logout">退出</el-button>
-    Home!
-  </div>
+  <el-container class="home-container">
+    <!-- 头部区域 -->
+    <el-header>
+      Header
+      <el-button type="info" @click="logout">退出</el-button>
+    </el-header>
+    <!-- 页面主体区域 -->
+    <el-container>
+      <!-- 侧边栏区域 -->
+      <el-aside width="200px">Aside</el-aside>
+      <!-- 右侧主体区域 -->
+      <el-main>Main</el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
@@ -18,4 +28,19 @@
 </script>
 
 <style lang="less" scoped>
+  .home-container {
+    height: 100%;
+  }
+
+  .el-header {
+    background-color: #373d41;
+  }
+
+  .el-aside {
+    background-color: #333744;
+  }
+
+  .el-main {
+    background-color: #eaedf1;
+  }
 </style>
