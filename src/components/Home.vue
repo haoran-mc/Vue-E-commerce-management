113f1f8861f74f5b82b310c045b87b377766c876
haoran <haoran.mc@outlook.com>
2021-08-24 11:38:31 +0800

    :lipstick:Home header

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,7 +2,10 @@
   <el-container class="home-container">
     <!-- 头部区域 -->
     <el-header>
-      Header
+      <div>
+        <img src="../assets/header-logo.png" alt="" />
+        <span>电商管理系统</span>
+      </div>
       <el-button type="info" @click="logout">退出</el-button>
     </el-header>
     <!-- 页面主体区域 -->
@@ -34,6 +37,19 @@
 
   .el-header {
     background-color: #373d41;
+    display: flex;
+    justify-content: space-between;
+    padding-left: 0;
+    align-items: center;
+    color: #fff;
+    font-size: 20px;
+    > div {
+      display: flex;
+      align-items: center;
+      span {
+        margin-left: 15px;
+      }
+    }
   }
 
   .el-aside {
