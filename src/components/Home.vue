db7d5cdb9089ec2b209b45556adb8e48c8d41fb3
haoran <haoran.mc@outlook.com>
2021-08-25 12:43:21 +0800

    :sparkles:|||

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -11,15 +11,23 @@
     <!-- 页面主体区域 -->
     <el-container>
       <!-- 侧边栏区域 -->
-      <el-aside width="200px">
+      <el-aside :width="isCollapse ? '64px' : '200px'">
+        <div class="toggle-button" @click="toggleCollapse">|||</div>
         <!-- 侧边栏菜单区域 -->
-        <el-menu background-color="#333744" text-color="#fff" active-text-color="#ffd04b">
+        <el-menu
+          background-color="#333744"
+          text-color="#fff"
+          active-text-color="#409eff"
+          unique-opened
+          :collapse="isCollapse"
+          :collapse-transition="false"
+        >
           <!-- 一级菜单 -->
           <el-submenu :index="item.id + ''" v-for="item in menulist" :key="item.id">
             <!-- 一级菜单模板区域 -->
             <template slot="title">
               <!-- 图标 -->
-              <i class="el-icon-location"></i>
+              <i :class="iconsObj[item.id]"></i>
               <!-- 文本 -->
               <span>{{item.authName}}</span>
             </template>
@@ -28,7 +36,7 @@
             <el-menu-item :index="subItem.id + ''" v-for="subItem in item.children" :key="subItem.id">
               <template slot="title">
                 <!-- 图标 -->
-                <i class="el-icon-location"></i>
+                <i class="el-icon-menu"></i>
                 <!-- 文本 -->
                 <span>{{subItem.authName}}</span>
               </template>
@@ -48,7 +56,16 @@
     data () {
       return {
         // 左侧菜单数据
-        menulist: []
+        menulist: [],
+        iconsObj: {
+          125: 'iconfont icon-user',
+          103: 'iconfont icon-tijikongjian',
+          101: 'iconfont icon-shangpin',
+          102: 'iconfont icon-danju',
+          145: 'iconfont icon-baobiao'
+        },
+        // 是否折叠
+        isCollapse: false
       }
     },
     created () {
@@ -67,6 +84,11 @@
         if (res.meta.status !== 200) return this.$message.error()
         this.menulist = res.data
         /* console.log(res) */
+      },
+
+      // 点击按钮，切换菜单的折叠与展开
+      toggleCollapse () {
+        this.isCollapse = !this.isCollapse
       }
     }
   }
@@ -96,9 +118,26 @@
 
   .el-aside {
     background-color: #333744;
+    .el-menu {
+      border-right: none;
+    }
   }
 
   .el-main {
     background-color: #eaedf1;
+  }
+
+  .iconfont {
+    margin-right: 10px;
+  }
+
+  .toggle-button {
+    background-color: #4a5064;
+    font-size: 10px;
+    line-height: 24px;
+    color: #fff;
+    text-align: center;
+    letter-spacing: 0.2em;
+    cursor: pointer;
   }
 </style>
